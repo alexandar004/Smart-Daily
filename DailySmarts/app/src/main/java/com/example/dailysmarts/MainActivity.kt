25b49d5c312d77c08ad4341a448040358ac2ef5f
Alexander <adamqnov07@gmail.com>
2022-02-02 17:13:28 +0200

    add-toolbar

---
+++ DailySmarts/app/src/main/java/com/example/dailysmarts/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.example.dailysmarts
+
+import android.os.Bundle
+import android.view.Menu
+import androidx.appcompat.app.AppCompatActivity
+import com.example.dailysmarts.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.toolbar, menu)
+        return true
+    }
+}
