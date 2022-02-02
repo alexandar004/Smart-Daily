ec543ef0847417d0e9ab4ac660adde591b5eae3d
Alexander <adamqnov07@gmail.com>
2022-02-02 17:33:34 +0200

    remove titleBar

--- DailySmarts/app/src/main/java/com/example/dailysmarts/MainActivity.kt
+++ DailySmarts/app/src/main/java/com/example/dailysmarts/MainActivity.kt
@@ -13,6 +13,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        supportActionBar?.hide()
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
